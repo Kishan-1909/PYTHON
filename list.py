531139d2b36596397c83e93bd7f3f8f1dda98226
Kishan <61969778+Kishan-1909@users.noreply.github.com>
2020-04-30 10:40:58 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,15 @@
+l=['a','b','c','d']
+print(l)
+d={
+    'a':'Apple',
+    'b':'Ball',
+    'c':'cat',
+    'd':'dog'
+}
+print(d)
+print(d["c"])
+t=("a","b","c","d","e")
+print(t)
+
+s={1,202,3,423,5,2011,435678,9,0}
+print(s)
